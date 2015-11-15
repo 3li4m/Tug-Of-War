60cb94f9dc0b02eb6b396b88792259e5f2639b60
Billy Mihalarias <bmihalarias@gmail.com>
2015-11-15 11:52:54 +1100

    Added in an ability class to allow for wizards and healers.

---
+++ TUG/Assets/Scripts/UnitClasses/Ablility.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+/// <summary>
+/// Ablility class.
+/// intened fire the "onCast" event when off cooldown,
+/// the cooldown is impimented. Call cast when spell  is to be fired.
+/// </summary>
+public abstract class Ablility 
+{
+	public float coolDown =2f;
+	private bool ready = true;
+	private float nextReady = - 250;//so the spell will be ready when the game starts
+
+	protected abstract void onCast(); //suceeded cast effect
+
+	protected void cast()//try to cast
+	{
+		checkReady();
+		if(ready)
+		{
+			onCast();
+			nextReady = Time.time + coolDown;
+		}
+
+	}
+
+	private void checkReady()
+	{
+		if(nextReady <= Time.time)//if we are past the point in time the spell is ready
+		{
+			ready = true;
+		}
+	}
+}
